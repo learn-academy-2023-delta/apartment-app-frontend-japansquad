631849881c50ccfe1efb9be0c210513805ce42ef
Phalco1 <elphalco1@gmail.com>
2023-07-07 12:07:52 -0500

    home-not-found

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='home-style'>
+        <div className="welcome-box">
+          <h1 className='title'>Welcome!</h1>
+          <p className='greeting'>welcome to japan</p>
+        </div>
+      </div>
+    </>
   )
 }
+
 
 export default Home
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+    <div className='pagefoundnot'>NotFound</div>
+    </>
   )
 }
 
