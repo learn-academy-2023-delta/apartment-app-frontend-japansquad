21b8340d2ed4e6f13276a70c7ac8bc12a5ebe92c
Phalco1 <elphalco1@gmail.com>
2023-07-07 11:41:54 -0500

    blah

---
+++ src/__Test__/Home.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home';
+
+describe("<Home />", () => {
+    it("has a header text", () => {
+        render(
+            <BrowserRouter>
+                <Home />
+            </BrowserRouter>
+        )
+        const header = screen.getByRole('heading', {
+            name: /welcome!/i
+          })
+        expect(header).toBeInTheDocument()
+    })
+})
