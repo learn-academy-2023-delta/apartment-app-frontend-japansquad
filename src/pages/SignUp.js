0c58f0b319084b5e798e0c3a9150e55a353e09c3
bShinzato <bshinzato@outlook.com>
2023-07-07 09:59:14 -0700

    for michael

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+
+  const formRef = useRef()
+  const navigate = useNavigate ()
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  
+    const formData = new FormData(formRef.current)
+    const data = Object.fromEntires(formData)
+    const userInfo = {
+      "user" : {email: data.email, password: data.password}
+    }
+    console.log(userInfo)
+    navigate("/")
+    e.target.reset()
+  }
   return (
-    <div>SignUp</div>
+    <>
+      <form ref={formRef} >
+        Email: <input type="email" name="email" placeholder="ex@example.com"/>
+        Password: <input type="password" name="password" placeholder='password'/>
+        Confirm Password: <input type="password" name="password_confirmation" placeholder='confirm password'/>
+        <input type='submit' value="Submit" />
+      </form>
+    </>
   )
 }
 
